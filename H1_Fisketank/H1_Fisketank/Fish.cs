87b48808ccbfc4e4f019a3c0e00b901974a55513
JakobEsp <jakobespersen0@gmail.com>
2023-01-26 09:12:16 +0100

    Progress

---
+++ H1_Fisketank/H1_Fisketank/Fish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Fisketank
+{
+    class Fish
+    {
+        public string name { get; set; } = "No name yet";
+        public int age { get; set; }
+        public string species { get; set; }
+        public double lenght { get; set; }
+        public double weight { get; set; }
+        public string[] types { get; set; }
+    }
+    public class Aquarium
+    {
+        List<Fish> fishList= new();
+    }
+}
---
+++ H1_Fisketank/H1_Fisketank/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Fisketank
+{
+    class Main
+    {
+        string[] type = { "Saltwater Fish", "Freshwater Fish", "Carnivore", "Herbivore" };
+
+        List<Fish> saltHerbs = new();
+        List<Fish> saltCarns = new();
+        List<Fish> freshHerbs = new();
+        List<Fish> freshCarns = new();
+
+        public void Menu()
+        {
+            
+        }
+        public void AddFish()
+        {
+
+        }
+        public void RemoveFish() 
+        {
+
+        }
+        public void ShowAquariums()
+        {
+            
+        }
+        //public void EditFish()
+        //{
+
+        //}
+    }
+}
